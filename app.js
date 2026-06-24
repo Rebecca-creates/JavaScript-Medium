65d0c62cc624aebfea71eb78ab19c419f4f8f78f
Rebecca-creates <rebeccajchaney@icloud.com>
2026-06-24 18:44:04 -0400

    Q5

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 /* Q1: Given two values, return the first one if it is false, otherwise return the second one. */
+
 function filterOutFalsy(elem1, elem2) {
   return !elem1 ? elem1 : elem2;
 }
@@ -6,6 +7,7 @@ function filterOutFalsy(elem1, elem2) {
 console.log(filterOutFalsy(0, 500));
 
 /* Q2: Return the length of any given array */
+
 function arrLength(arr) {
   return arr.length;
 }
@@ -13,6 +15,7 @@ function arrLength(arr) {
 console.log(arrLength([1, 2, 3, 4]));
 
 /* Q3: Get the last element in an array */
+
 function arrLength(arr) {
   return arr.length - 1;
 }
@@ -20,6 +23,7 @@ function arrLength(arr) {
 console.log(arrLength([0, 1, 2, 3]));
 
 /* Q4: Given an array, return the sum of every element */
+
 function arrSum(arr) {
   let sum = 0;
   for (let i = 0; i < arr.length; ++i) {
@@ -31,6 +35,7 @@ function arrSum(arr) {
 console.log(arrSum([2, 2, 2]));
 
 /* Q5: Add up the numbers from a single number - Given a number, add up all th numbers from one to the number that is given Ex. An input of 4 will give you 1+2+3+4=10 */
+
 function progressiveSum(num) {
   let sum = 0;
   for (let i = 1; i <= num; ++i) {
@@ -40,3 +45,5 @@ function progressiveSum(num) {
 }
 
 console.log(progressiveSum([4]));
+
+/* Q6:  */
